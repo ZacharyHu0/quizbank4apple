ca3f607132b1a1d9c43bc69b3def388d4a2eb3ad
hao <huzh@zju.edu.cn>
2025-11-11 04:35:32 +0800

    Initial Commit

---
+++ quizbank4apple/quizbank4appleApp.swift
@@ -0,0 +1,17 @@
+//
+//  quizbank4appleApp.swift
+//  quizbank4apple
+//
+//  Created by zihao hu on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct quizbank4appleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
